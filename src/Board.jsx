cec8f355a00bb9b8c99774c6e2e447c8cdb98c8f
Zhivko Shopov <zhishopov@gmail.com>
2025-03-12 17:56:57 +0000

    Add calculate winner helper function

--- src/Board.jsx
+++ src/Board.jsx
@@ -8,6 +8,8 @@ export default function Board() {
   function clickHandler(i) {
     const nextSquares = squares.slice();
 
+    if (squares[i] || calculateWinner(squares)) return;
+
     if (xIsNext) {
       nextSquares[i] = "X";
     } else {
@@ -64,4 +66,26 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  // Winning combinations
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
