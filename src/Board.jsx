3a365acb3a7e20771e49db063c3578b69843ca3f
Zhivko Shopov <zhishopov@gmail.com>
2025-03-12 17:51:42 +0000

    Add state for next player

--- src/Board.jsx
+++ src/Board.jsx
@@ -3,11 +3,19 @@ import Square from "./Square";
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
   function clickHandler(i) {
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
