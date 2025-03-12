bdd73766f24825d8e204ae4483ce1742a5fa8093
Zhivko Shopov <zhishopov@gmail.com>
2025-03-12 17:36:49 +0000

    Lifting state

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,22 +1,31 @@
+import { useState } from "react";
 import Square from "./Square";
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function clickHandler() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square value="1"></Square>
-        <Square value="2"></Square>
-        <Square value="3"></Square>
+        <Square value={squares[0]} onSquareClick={clickHandler}></Square>
+        <Square value={squares[1]}></Square>
+        <Square value={squares[2]}></Square>
       </div>
       <div className="board-row">
-        <Square value="4"></Square>
-        <Square value="5"></Square>
-        <Square value="6"></Square>
+        <Square value={squares[3]}></Square>
+        <Square value={squares[4]}></Square>
+        <Square value={squares[5]}></Square>
       </div>
       <div className="board-row">
-        <Square value="7"></Square>
-        <Square value="8"></Square>
-        <Square value="9"></Square>
+        <Square value={squares[6]}></Square>
+        <Square value={squares[7]}></Square>
+        <Square value={squares[8]}></Square>
       </div>
     </>
   );
